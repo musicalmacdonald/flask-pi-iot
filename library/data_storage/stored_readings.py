33977d6bc8cb78dbaff64366ae73a8d720cf374b
Megan <musicalmacdonald@gmail.com>
2018-09-02 12:34:06 -0600

    test add readings withLoc and without, timing added to unnittest

--- library/data_storage/stored_readings.py
+++ library/data_storage/stored_readings.py
@@ -3,11 +3,22 @@
 import pandas as pd
 import numpy as np
 
+
 class StoredReadings():
     def __init__(self):
         #empty dataframe
-        df = pd.DataFrame(columns=['serial-no','timestamp', 'x', 'y', 'z'])
-
+        self.df = pd.DataFrame(columns=['serial-no','timestamp', 'x', 'y', 'z'])
 
     def add_readings(self, serial_no, timestamp, x, y, z):
-        self.df.append({'serial-no': serial_no,'timestamp':timestamp, 'x': x, 'y':y, 'z':z}, ignore_index=True)
+        self.df = self.df.append({'serial-no': serial_no, 'timestamp' :timestamp, 'x': x, 'y':y, 'z':z}, ignore_index=True)
+
+    def add_readings_withLoc(self, serial_no, timestamp, x, y, z):
+        self.df.loc[self.df.shape[0]] = {'serial-no': serial_no, 'timestamp': timestamp, 'x': x, 'y': y, 'z': z}
+
+    def get_number_of_readings(self):
+        n = self.df.shape[0]
+        return n
+
+
+if __name__ == '__main__':
+    print("running Stored Readings")
--- library/data_storage/test_stored_readings.py
+++ library/data_storage/test_stored_readings.py
@@ -6,22 +6,55 @@
 
 import unittest
 import datetime
-import StoredReadings from stored_readings
+import time
+import random
+from .stored_readings import StoredReadings
+
 
 class TestStoredReadings(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_add_readings(self):
-        print("Starting ADD readings test")
+        starttime=time.time()
         aSR = StoredReadings()
 
         # Create data to send
-        for i in range(0, 3):
+        for i in range(0, 1000):
             x = random.randint(0, 358)
             y = random.randint(0, 358)
             z = random.randint(0, 358)
             aSR.add_readings("46406064", datetime, x, y, z)
 
         n = aSR.get_number_of_readings()
-        self.assertTrue(n == 3)
+        self.assertTrue(n == 1000)
+        endtime=time.time()
+
+        elapsedtime= endtime - starttime
+        print("test time: {}".format(elapsedtime))
+
+    def test_add_readings_withLoc(self):
+        starttime=time.time()
+
+        aSR = StoredReadings()
+
+        # Create data to send
+        for i in range(0, 1000):
+            x = random.randint(0, 358)
+            y = random.randint(0, 358)
+            z = random.randint(0, 358)
+            aSR.add_readings_withLoc("46406064", datetime, x, y, z)
+
+        n = aSR.get_number_of_readings()
+        print(n)
+        self.assertTrue(n == 1000)
+
+        endtime = time.time()
+        elapsedtime = endtime - starttime
+        print("Loc test time is: {}".format(elapsedtime))
+
+
+
+if __name__ == '__main__':
+    print("starting tests")
+    unittest.main()
