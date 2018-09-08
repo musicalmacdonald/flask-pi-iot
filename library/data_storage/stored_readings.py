b9e70af7c3d5e1899e1a8174dc674f22fd2431be
Megan <musicalmacdonald@gmail.com>
2018-09-08 16:22:44 -0600

    added functionality to print readings and filter by serial number with associated unittests

--- library/data_storage/stored_readings.py
+++ library/data_storage/stored_readings.py
@@ -7,17 +7,25 @@ import numpy as np
 class StoredReadings():
     def __init__(self):
         #empty dataframe
-        self.df = pd.DataFrame(columns=['serial-no','timestamp', 'x', 'y', 'z'])
+        self.df = pd.DataFrame(columns=['serial_no','timestamp', 'x', 'y', 'z'])
 
     def add_readings(self, serial_no, timestamp, x, y, z):
-        self.df = self.df.append({'serial-no': serial_no, 'timestamp' :timestamp, 'x': x, 'y':y, 'z':z}, ignore_index=True)
+        self.df = self.df.append({'serial_no': serial_no, 'timestamp' :timestamp, 'x': x, 'y':y, 'z':z}, ignore_index=True)
 
     def add_readings_withLoc(self, serial_no, timestamp, x, y, z):
-        self.df.loc[self.df.shape[0]] = {'serial-no': serial_no, 'timestamp': timestamp, 'x': x, 'y': y, 'z': z}
+        self.df.loc[self.df.shape[0]] = {'serial_no': serial_no, 'timestamp': timestamp, 'x': x, 'y': y, 'z': z}
 
     def get_number_of_readings(self):
         n = self.df.shape[0]
         return n
+
+    def list_readings(self):
+        return self.df
+
+    def get_readings_by_serial(self, serial):
+        return self.df.query('serial_no == {}'.format(serial))
+
+
 
 
 if __name__ == '__main__':
--- library/data_storage/test_stored_readings.py
+++ library/data_storage/test_stored_readings.py
@@ -20,14 +20,14 @@ class TestStoredReadings(unittest.TestCase):
         aSR = StoredReadings()
 
         # Create data to send
-        for i in range(0, 1000):
+        for i in range(0, 3):
             x = random.randint(0, 358)
             y = random.randint(0, 358)
             z = random.randint(0, 358)
             aSR.add_readings("46406064", datetime, x, y, z)
 
         n = aSR.get_number_of_readings()
-        self.assertTrue(n == 1000)
+        self.assertTrue(n == 3)
         endtime=time.time()
 
         elapsedtime= endtime - starttime
@@ -39,19 +39,58 @@ class TestStoredReadings(unittest.TestCase):
         aSR = StoredReadings()
 
         # Create data to send
-        for i in range(0, 1000):
+        for i in range(0, 3):
             x = random.randint(0, 358)
             y = random.randint(0, 358)
             z = random.randint(0, 358)
-            aSR.add_readings_withLoc("46406064", datetime, x, y, z)
+            date = datetime.datetime
+            aSR.add_readings_withLoc("46406064", 'date', x, y, z)
 
         n = aSR.get_number_of_readings()
         print(n)
-        self.assertTrue(n == 1000)
+        self.assertTrue(n == 3)
 
         endtime = time.time()
         elapsedtime = endtime - starttime
         print("Loc test time is: {}".format(elapsedtime))
+
+    def test_list_readings(self):
+
+        aSR = StoredReadings()
+
+        # Create data to send
+        for i in range(0, 3):
+            x = random.randint(0, 358)
+            y = random.randint(0, 358)
+            z = random.randint(0, 358)
+            date = datetime.datetime
+            aSR.add_readings("46406064", 'date', x, y, z)
+
+        print(aSR.list_readings())
+
+    def test_get_readings_by_serial(self):
+
+        aSR= StoredReadings()
+        serialNo=['MEGAN0000000', 'SHANE0000000', 250]
+
+        for i in range(0, 3):
+            x = random.randint(0, 358)
+            y = random.randint(0, 358)
+            z = random.randint(0, 358)
+            serialNumber = serialNo[i]
+            date = datetime.datetime
+            aSR.add_readings(serialNumber, 'date', x, y, z)
+
+        print(aSR.list_readings())
+        serialList = aSR.get_readings_by_serial('250')
+        print(serialList)
+        print(len(serialList))
+        #n = serialList.get_number_of_readings()
+        print('n is: {}'.format(n))
+        self.assertTrue(n == 1)
+
+
+
 
 
 
